d3b1b2c074faf489c534c0c3f922e1a077f99258
WeiJun0827 <weijunhung@outlook.com>
2022-06-04 00:23:29 +0800

    🍱 Load background images from local

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
 $('#backstretch-container').backstretch([
-  'https://dn2q0givzyynx.cloudfront.net/Taipei%20Travel%20Time/bg-bus.jpg',
-  'https://dn2q0givzyynx.cloudfront.net/Taipei%20Travel%20Time/bg-street.jpg',
-  'https://dn2q0givzyynx.cloudfront.net/Taipei%20Travel%20Time/bg-metro.jpg',
-  'https://dn2q0givzyynx.cloudfront.net/Taipei%20Travel%20Time/bg-skyline.jpg',
+  './assets/img/bg-bus.jpg',
+  './assets/img/bg-street.jpg',
+  './assets/img/bg-metro.jpg',
+  './assets/img/bg-skyline.jpg',
 ], {
   fade: 1500, // 動畫時長
   duration: 3000, // 切換延時
